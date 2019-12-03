494b1382949268fa5b11e1e809da751bf8bf4e5b
MP_Praewrung <38246225+meipraewrung@users.noreply.github.com>
2019-12-03 18:26:51 +0700

    Up 9

--- api.js
+++ api.js
@@ -57,7 +57,11 @@ function deleteProductById(req, res) {
 function addProduct(req, res) {
     var payload = req.body
     // #9 Add a new product 
-
+    var product = new Product(payload);
+    product.save(function(err){
+        if(err) res.status(500).json(err);
+        res.json({status: "Added a product"});
+    });
     // ===============================
 }
 
