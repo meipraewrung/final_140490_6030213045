84c4ce0435b04c40c5d46c1b0535fdfcfd7e0f1b
MP_Praewrung <38246225+meipraewrung@users.noreply.github.com>
2019-12-03 18:21:23 +0700

    Up 7

--- api.js
+++ api.js
@@ -33,6 +33,10 @@ function updateProductById(req, res) {
     var payload = req.body
     var pid = req.params.pid;    
     // #7 Update a product by ID (findByIdAndUpdate)
+    Product.findByIdAndUpdate(payload,pid,function(err){
+        if(err) res.status(500).json({ status: "error", message: err});
+        res.json({status:"Update a product"})
+    })
 
     // ===============================
 }
